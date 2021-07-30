75cdccbe83a36180d1624b5185d90c2774f19aa0
Hitesh <68400085+hsevlia@users.noreply.github.com>
2021-07-30 14:54:35 +0530

    whole project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.myapplication;
+
+import static android.view.View.*;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.document.documents;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    StorageReference storageReference ;
+    DatabaseReference databaseReference;
+    ProgressDialog progressDialog;
+
+
+    Button UploadButton;
+    CardView SelectButton;
+    EditText filename ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+         progressDialog = new ProgressDialog(this);
+
+        UploadButton = findViewById(R.id.upload);
+        SelectButton = findViewById(R.id.select);
+        filename = findViewById(R.id.filename);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploadPDF");
+
+
+        UploadButton.setEnabled(false);
+
+        SelectButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                selectPDF();
+
+
+            }
+        });
+
+
+
+    }
+
+    private void selectPDF() {
+
+
+        Intent intent = new Intent();
+                intent.setType("application/pdf");
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"PDF FILE SELECT"),12);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==12&&resultCode ==RESULT_OK&& data!= null &&data.getData()!=null)
+        {
+
+            UploadButton.setEnabled(true);
+            filename.setText(data.getDataString().substring(data.getDataString().lastIndexOf("/")+1));
+
+            UploadButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+                    documents.uploadPDFFileFirebase(data.getData(),filename,progressDialog);
+                    Toast.makeText(MainActivity.this,"File Uploaded",Toast.LENGTH_LONG).show();
+
+                }
+            });
+
+
+        }
+    }
+
+
+}
---
+++ document/src/main/java/com/example/document/documents.java
@@ -0,0 +1,68 @@
+package com.example.document;
+
+import android.app.ProgressDialog;
+import android.net.Uri;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class documents {
+
+
+    public static void uploadPDFFileFirebase(Uri data, EditText filename, ProgressDialog progressDialog) {
+
+
+
+        StorageReference storageReference ;
+        DatabaseReference databaseReference;
+
+
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploadPDF");
+
+
+
+        progressDialog.setTitle("Uploading...");
+        progressDialog.show();
+        StorageReference reference = storageReference.child("upload"+System.currentTimeMillis()+".pdf");
+        reference.putFile(data)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                        while(!uriTask.isComplete());
+                        Uri uri = uriTask.getResult();
+
+                        putPDF putPDF = new putPDF(filename.getText().toString(),uri.toString());
+                        databaseReference.child(databaseReference.push().getKey()).setValue(putPDF);
+                        progressDialog.dismiss();
+
+
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+
+                double progress = (100.0* snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                progressDialog.setMessage("File Uploading... "+(int) progress+"%");
+
+            }
+        });
+    }
+
+
+
+
+
+
+}
